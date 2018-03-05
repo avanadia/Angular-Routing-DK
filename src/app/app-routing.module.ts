adc40c0318d9d8fd7ca9c6c46302c817f3cf0e02
Anthony <nin0@nin0.com>
2018-03-05 16:19:06 -0500

    created custom loading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,24 @@ import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { AuthGuard } from './user/auth-guard.service'; 
+import { SelectiveStrategy } from './selective-strategy.service';
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
             { path: 'welcome', component: WelcomeComponent },
-            { path: 'products', canActivate: [ AuthGuard ], canLoad: [ AuthGuard ], loadChildren: 'app/products/product.module#ProductModule' },
+            // canLoad guard blocks preloading
+            { 
+                path: 'products', 
+                canActivate: [ AuthGuard ], 
+                data: { preload: false },
+                loadChildren: 'app/products/product.module#ProductModule' 
+            },
             { path: '', redirectTo: 'welcome', pathMatch: 'full' },
             { path: '**', component: PageNotFoundComponent },            
-          ], { enableTracing: false })
+          ], { enableTracing: false, preloadingStrategy: SelectiveStrategy })
     ],
+    providers: [ SelectiveStrategy ],
     exports: [ RouterModule ]
 })
 
